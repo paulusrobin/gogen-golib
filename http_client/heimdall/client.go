704a69e7a5f7ccbeea21277bd13ec404bbb18ce8
Paulus Robin <paulusrobin@Robins-MacBook-Pro-Personal.local>
2022-07-08 12:38:33 +0400

    feat(httpclient): add heimdall implementation

---
+++ http_client/heimdall/client.go
@@ -0,0 +1,28 @@
+package heimdall
+
+import (
+	"context"
+	"github.com/gojek/heimdall/v7"
+	heimdallhttpclient "github.com/gojek/heimdall/v7/httpclient"
+	heimdallhystrixclient "github.com/gojek/heimdall/v7/hystrix"
+	httpclient "github.com/paulusrobin/gogen-lib/http_client"
+	"net/http"
+)
+
+type client struct {
+	c heimdall.Client
+}
+
+func (c client) Do(ctx context.Context, request *http.Request) (*http.Response, error) {
+	return c.c.Do(request)
+}
+
+// NewHttpClient function to initialize circuit heimdall http.
+func NewHttpClient(options ...heimdallhttpclient.Option) httpclient.Client {
+	return &client{heimdallhttpclient.NewClient(options...)}
+}
+
+// NewHystrixClient function to initialize circuit heimdall hystrix.
+func NewHystrixClient(options ...heimdallhystrixclient.Option) httpclient.Client {
+	return &client{heimdallhystrixclient.NewClient(options...)}
+}
--- http_client/mock/client.go
+++ http_client/mock/client.go
@@ -7,20 +7,18 @@ import (
 )
 
 type (
-	response struct {
-		response *http.Response
-		err      error
-	}
 	client struct {
 		response *http.Response
 		err      error
 	}
 )
 
+// Do function implements httpclient.Client.
 func (c client) Do(ctx context.Context, request *http.Request) (*http.Response, error) {
 	return c.response, c.err
 }
 
+// NewMockHTTPClient function to initialize httpclient.mock
 func NewMockHTTPClient(response *http.Response, err error) httpclient.Client {
 	return client{response: response, err: err}
 }
