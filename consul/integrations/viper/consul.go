deab3d0e6b1793a93165fe16a73a63526419c7d4
Paulus Robin <paulusrobin@Robins-MacBook-Pro-Personal.local>
2022-07-09 08:01:03 +0400

    feature(consul): add viper consul

--- consul/integrations/viper/consul.go
+++ consul/integrations/viper/consul.go
@@ -8,42 +8,72 @@ import (
 	"time"
 )
 
+const configProvider = `consul`
+
 type consulInstance struct {
+	cfg      consul.Config
 	reloader *viper.Viper
-	consul.Config
 }
 
-func NewConsulReader() {
-	viper.New()
+// NewConsulReader functions to initialize consul.Reader.
+func NewConsulReader(cfg consul.Config) (consul.Reader, error) {
+	reloader := viper.New()
+	cfg.SetConsulRemote()
+	if err := reloader.AddRemoteProvider(configProvider, cfg.GetConsulRemote(), cfg.Connection.Key); err != nil {
+		log.Error().Err(err).Msgf("cannot read remote config from consul for key: %s", cfg.Connection.Key)
+		return nil, err
+	}
+	reloader.SetConfigType(cfg.ConfigType)
+	return &consulInstance{
+		cfg:      cfg,
+		reloader: reloader,
+	}, nil
 }
 
-func (c consulInstance) Read(key string, data interface{}) error {
-	param.cfg.setConsulToken()
-	if err := c.reloader.AddRemoteProvider(remoteConfigProvider, param.cfg.getConsulRemote(), param.key); err != nil {
-		log.Error().Err(err).Msgf("cannot read remote config from consul for key: %s", param.key)
+// Read functions to read consul data implement consul.Reader.
+func (c consulInstance) Read(data interface{}) error {
+	if err := c.reloader.ReadRemoteConfig(); err != nil {
+		log.Error().Err(err).
+			Fields(map[string]interface{}{"config": c.cfg}).
+			Msgf("cannot read remote config")
 		return err
 	}
 
-	c.reloader.SetConfigType(param.configType)
-	if err := hotReloadViper.ReadRemoteConfig(); err != nil {
-		param.onError(param.key, err)
-	} else {
-		param.onSuccess(param.key, hotReloadViper)
+	if err := c.reloader.Unmarshal(&data); err != nil {
+		log.Error().Err(err).
+			Fields(map[string]interface{}{"config": c.cfg}).
+			Msgf("cannot unmarshall remote config to data")
+		return err
 	}
 
-	log.Info().Msgf("remote config %s was read successfully from consul. starting periodical updates...", param.key)
+	log.Info().
+		Fields(map[string]interface{}{"config": c.cfg, "data": data}).
+		Msgf("success read remote config %s from consul", c.cfg.Connection.Key)
+
+	if c.cfg.Interval <= 0 {
+		return nil
+	}
+
+	log.Info().Msgf("starting remote config %s periodical updates...", c.cfg.Connection.Key)
 
 	// open a goroutine to watch remote changes forever
 	go func() {
 		for {
-			time.Sleep(param.interval)
+			time.Sleep(c.cfg.Interval)
 
-			if err := hotReloadViper.WatchRemoteConfig(); err != nil {
-				param.onError(param.key, err)
+			if err := c.reloader.WatchRemoteConfig(); err != nil {
+				log.Warn().Err(err).
+					Fields(map[string]interface{}{"config": c.cfg, "data": data}).
+					Msgf("cannot watch remote config, using previous config")
 				continue
 			}
 
-			param.onSuccess(param.key, hotReloadViper)
+			if err := c.reloader.Unmarshal(&data); err != nil {
+				log.Warn().Err(err).
+					Fields(map[string]interface{}{"config": c.cfg, "data": data}).
+					Msgf("cannot unmarshall periodical remote config to data, using previous config")
+				continue
+			}
 		}
 	}()
 	return nil
--- consul/interface/consul.go
+++ consul/interface/consul.go
@@ -4,30 +4,43 @@ import (
 	"fmt"
 	"os"
 	"strconv"
+	"time"
 )
 
 type (
-	Config struct {
+	ConnectionConfig struct {
 		Host  string
 		Port  int
 		Token string
+		Key   string
+	}
+	Config struct {
+		Connection ConnectionConfig
+		ConfigType string
+		Interval   time.Duration
 	}
 	Reader interface {
-		Read(key string, data interface{}) error
+		Read(data interface{}) error
 	}
 )
 
-func (c *Config) GetConsulRemote() string {
-	if c.Host == "" {
-		c.Host = os.Getenv("CONSUL_HOST")
+func (c *Config) SetConsulRemote() {
+	if os.Getenv("CONSUL_HTTP_TOKEN") == "" && c.Connection.Token != "" {
+		_ = os.Setenv("CONSUL_HTTP_TOKEN", c.Connection.Token)
 	}
-	if c.Port == 0 {
+}
+
+func (c *Config) GetConsulRemote() string {
+	if c.Connection.Host == "" {
+		c.Connection.Host = os.Getenv("CONSUL_HOST")
+	}
+	if c.Connection.Port == 0 {
 		consulPort := os.Getenv("CONSUL_PORT")
 		if consulPort != "" {
 			if port, err := strconv.Atoi(consulPort); err == nil {
-				c.Port = port
+				c.Connection.Port = port
 			}
 		}
 	}
-	return fmt.Sprintf("%s:%d", c.Host, c.Port)
+	return fmt.Sprintf("%s:%d", c.Connection.Host, c.Connection.Port)
 }
