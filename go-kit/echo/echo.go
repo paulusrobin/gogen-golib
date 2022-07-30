ca479d18bd302ff9d7ad1ec022febc5a407d2264
Paulus Robin <paulus.robin@hotmail.com>
2022-07-30 11:30:07 +0400

    feat(go-kit): add echo go-kit

--- go-kit/echo/echo.go
+++ go-kit/echo/echo.go
@@ -3,40 +3,14 @@ package echo
 import (
 	"github.com/go-kit/kit/endpoint"
 	"github.com/labstack/echo/v4"
-	"net/http"
-)
-
-type (
-	option interface {
-		Apply(o *options)
-	}
-	options struct {
-		encoder      func(c echo.Context, response interface{}) error
-		decoder      func(c echo.Context) (interface{}, error)
-		errorHandler func(c echo.Context, err error) error
-	}
-)
-
-var (
-	defaultDecoder = func(c echo.Context) (interface{}, error) {
-		return nil, nil
-	}
-	defaultEncoder = func(c echo.Context, response interface{}) error {
-		return c.JSON(http.StatusOK, response)
-	}
 )
 
 // Handler is function to adapt go-kit to echo http framework.
-func Handler(ep endpoint.Endpoint, opts ...option) func(c echo.Context) error {
-	var settings = options{
-		encoder:      defaultEncoder,
-		decoder:      defaultDecoder,
-		errorHandler: nil,
-	}
+func Handler(ep endpoint.Endpoint, opts ...Option) func(c echo.Context) error {
+	var settings = defaultOptions
 	for _, opt := range opts {
 		opt.Apply(&settings)
 	}
-
 	return func(c echo.Context) error {
 		errFunc := func(err error) error {
 			if settings.errorHandler != nil {
