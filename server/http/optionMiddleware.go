2ff8051dae1b5eb5ec88f782a7dd93fdda89f1af
Paulus Robin <paulus.robin@hotmail.com>
2022-09-30 10:44:08 +0400

    feat(server): add http server

--- server/http/optionMiddleware.go
+++ server/http/optionMiddleware.go
@@ -2,14 +2,19 @@ package http
 
 import "github.com/labstack/echo/v4"
 
-type registerMiddleware echo.MiddlewareFunc
+type registerMiddleware []echo.MiddlewareFunc
 
 // Apply function to implement Option.
 func (r registerMiddleware) Apply(o *options) {
-	o.middlewares = append(o.middlewares, echo.MiddlewareFunc(r))
+	o.middlewares = append(o.middlewares, r...)
 }
 
 // RegisterMiddleware function to add middleware to server.
 func RegisterMiddleware(fn echo.MiddlewareFunc) Option {
+	return registerMiddleware([]echo.MiddlewareFunc{fn})
+}
+
+// RegisterMiddlewares function to add middleware to server.
+func RegisterMiddlewares(fn []echo.MiddlewareFunc) Option {
 	return registerMiddleware(fn)
 }
